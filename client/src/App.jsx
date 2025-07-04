1bb7dbb2ebfa1f1b9fcece67087a1da95eaeed90
Vaibhao Hatwar <vaibhaohatwar.works@gmail.com>
2025-07-05 01:23:02 +0530

    feat(client): integrate react-hot-toast for global notifications
    - Added <Toaster /> component to main.jsx for global toast support
    - Installed and configured react-hot-toast
    - Added test button in App.jsx to trigger success toast

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,20 @@
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/HomePage"
-import CreatePage from "./pages/CreatePage"
-import MemoDetailPage from "./pages/MemoDetailPage"
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import MemoDetailPage from "./pages/MemoDetailPage";
+import toast from "react-hot-toast";
 
 const App = () => {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/create" element={<CreatePage />} />
-      <Route path="/memo/:id" element={<MemoDetailPage />} />
-    </Routes>
+      <button onClick={() => toast.success("congrats!")}>Click Me!</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/memo/:id" element={<MemoDetailPage />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
