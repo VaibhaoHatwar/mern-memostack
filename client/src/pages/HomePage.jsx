e9170d6cdd85932df24095cd246af82037339878
Vaibhao Hatwar <vaibhaohatwar.works@gmail.com>
2025-07-05 20:35:34 +0530

    feat(client): render memos using MemoCard with styled background and date formatting
    - Added <MemoCard /> component to display memo title, content, and created date
    - Integrated MemoCard into HomePage and mapped dynamic memos
    - Fixed radial gradient background in App layout using Tailwind CSS
    - Added formatDate utility to display formatted creation date
    - Included icons (edit, delete) and routing for memo detail view

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,14 +1,54 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Navbar from "../components/Navbar"
 import RateLimitedUI from "../components/RateLimitedUI";
+import axios from "axios";
+import toast from "react-hot-toast"
+import MemoCard from "../components/MemoCard";
 
 const HomePage = () => {
-  const [isRateLimited, setIsRateLimited] = useState(true);
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [memos, setMemos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // 🧠 Fetch memos on page load
+  useEffect(() => {
+    const fetchMemos = async (params) => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/memos");
+        setMemos(res.data);
+        console.log(memos);
+        setIsRateLimited(false);
+      } catch (error) {
+        console.log("Error fetching memos");
+        console.log(error);
+        if (error.response?.status === 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error("Failed to load notes");
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchMemos();
+  }, [])
 
   return (
     <div className="min-h-screen">
       <Navbar />
       {isRateLimited && <RateLimitedUI />}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading memos...</div>}
+
+        {memos.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {memos.map((memo) => (
+              <MemoCard key={memo._id} memo={memo} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
